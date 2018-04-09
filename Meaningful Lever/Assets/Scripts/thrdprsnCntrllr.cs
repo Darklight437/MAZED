c15d50bd1abbb898076b6b326c848721140b74dd
Darklight437 <darklight437@gmail.com>
2018-04-10 09:21:32 +1000

    new 3rd person controller and metadata file fix

--- Meaningful Lever/Assets/Scripts/thrdprsnCntrllr.cs
+++ Meaningful Lever/Assets/Scripts/thrdprsnCntrllr.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 3rdpersonController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class thrdprsnCntrllr : MonoBehaviour {
+
+
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    CapsuleCollider m_Capsule;
+    bool m_IsGrounded;
+    float m_CapsuleHeight;
+    Vector3 m_CapsuleCenter;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Capsule = GetComponent<CapsuleCollider>();
+        m_CapsuleHeight = m_Capsule.height;
+        m_CapsuleCenter = m_Capsule.center;
+
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Move(Vector3 move, bool jump)
+    {
+
+        if (move.magnitude > 1f) move.Normalize();
+    }
+
 }
