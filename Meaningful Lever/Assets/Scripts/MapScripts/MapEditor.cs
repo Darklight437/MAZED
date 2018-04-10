188d4792191b51de5289cb4a5044565832147d97
Darklight437 <darklight437@gmail.com>
2018-04-10 10:56:05 +1000

    system update push test

---
+++ Meaningful Lever/Assets/Scripts/MapScripts/MapEditor.cs
@@ -0,0 +1,117 @@
+﻿#if (UNITY_EDITOR) 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapEditorScript))]
+public class MapEditor : Editor
+{
+
+    /*
+    * OnInspectorGUI
+    * virtual function override
+    * 
+    * this overrides the default inspector for or MapEditorScript allowing
+    * for our own custome buttons to be displayed there
+    * 
+    * @returns nothing
+    */
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MapEditorScript myScript = (MapEditorScript)target;
+
+        //adds in a button to generate a default map
+        //of normal tiles when clicked
+        if (GUILayout.Button("Generate base Map"))
+        {
+            myScript.GenerateBasMap();
+        }
+        
+        //this button calls a function in MapEditorScript to toggle on and off paint mode
+        if (GUILayout.Button("Toggle paint mode"))
+        {
+            myScript.TogglePaint();
+        }
+        
+        //these buttons will only display if paint mode is on
+        if (myScript.enablePaint)
+        {
+            
+            if (GUILayout.Button("Slect Empty"))
+            {
+                myScript.SetPaintTypeEmptyTile();
+            }
+            
+            if (GUILayout.Button("Slect Floor"))
+            {
+                myScript.SetPaintTypeFloorTile();
+            }
+            
+            if (GUILayout.Button("Slect Wall"))
+            {
+                myScript.SetPaintTypeWALLTile();
+            }
+            
+            if (GUILayout.Button("Slect Door"))
+            {
+                myScript.SetPaintTypeDoorTile();
+            }
+
+            if (GUILayout.Button("Slect Wall Font"))
+            {
+                myScript.SetPaintTypeWALLFONTTile();
+            }
+        }
+    }
+
+    /*
+    * OnSceneGUI
+    * 
+    * this function is where we override unitys mouse controls 
+    * to implament our own ones when in paint mode
+    * 
+    * @returns nothing
+    */
+    void OnSceneGUI()
+    {
+        MapEditorScript myScript = (MapEditorScript)target;
+
+        //gets the event stuff needed to override the mouse
+        Event e = Event.current;
+        int controlID = GUIUtility.GetControlID(FocusType.Passive);
+
+        //only overrides when enable paint is true
+        if (myScript.enablePaint)
+        {
+            //switch statements for the override
+            switch (e.GetTypeForControl(controlID))
+            {
+                //when we click down the paint
+                case EventType.MouseDown:
+                    GUIUtility.hotControl = controlID;
+                    myScript.Paint();
+                    e.Use();
+                    break;
+
+                case EventType.MouseUp:
+                    GUIUtility.hotControl = 0;
+                    e.Use();
+                    break;
+
+                //when we drag we also paint
+                case EventType.MouseDrag:
+                    GUIUtility.hotControl = controlID;
+                    myScript.Paint();
+                    e.Use();
+                    break;
+
+                case EventType.KeyDown:
+                    break;
+            }
+        }
+    }
+}
+#endif
---
+++ Meaningful Lever/Assets/Scripts/MapScripts/MapEditorScript.cs
@@ -0,0 +1,101 @@
+﻿#if (UNITY_EDITOR) 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapEditorScript : MonoBehaviour
+{
+    public GameObject[] m_paintType;
+    
+    public GameObject tileObj;
+    
+    public uint mapLength;
+    public uint mapWidth;
+    
+    public GameObject world;
+    
+    public bool enablePaint = false;
+    
+    public void GenerateBasMap()
+    {
+        world = new GameObject();
+        
+        world.name = "World GameObject";
+        world.tag = "World";
+
+        tileObj = m_paintType[1];
+
+
+        for (int x = 0; x < mapLength; x++)
+        {
+            for (int z = 0; z < mapWidth; z++)
+            {
+                GameObject mapTile = Instantiate(tileObj, new Vector3(x * 3, 0, z * 3), Quaternion.identity);
+                
+                mapTile.transform.SetParent(world.transform);
+            }
+        }
+    }
+
+    public void TogglePaint()
+    {
+        if (enablePaint == false)
+        {
+            enablePaint = true;
+        }
+        else
+        {
+            enablePaint = false;
+        }
+    }
+    
+    public void SetPaintTypeEmptyTile()
+    {
+        tileObj = m_paintType[0];
+    }
+
+    public void SetPaintTypeFloorTile()
+    {
+        tileObj = m_paintType[1];
+    }
+
+    public void SetPaintTypeWALLTile()
+    {
+        tileObj = m_paintType[2];
+    }
+
+    public void SetPaintTypeWALLFONTTile()
+    {
+        tileObj = m_paintType[3];
+    }
+
+    public void SetPaintTypeDoorTile()
+    {
+        tileObj = m_paintType[4];
+    }
+    
+    public void Paint()
+    {
+        if (Event.current.type == EventType.MouseDown)
+        {
+            RaycastHit hit;
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject go = Instantiate(tileObj, hit.collider.transform.position, Quaternion.identity);
+                go.transform.SetParent(world.transform);
+
+                Transform topTransform = hit.transform.parent != null ? hit.transform.parent : hit.transform;
+
+                foreach (Transform tran in topTransform)
+                {
+                    DestroyImmediate(hit.transform.gameObject);
+                }
+                DestroyImmediate(topTransform.gameObject);
+            }
+        }
+    }
+}
+#endif
