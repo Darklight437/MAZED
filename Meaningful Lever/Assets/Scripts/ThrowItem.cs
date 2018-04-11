1aefc9517c1477827339db8e1d469f9b4a568e9f
Callum <callum.dunstone@gmail.com>
2018-04-11 10:06:06 +1000

    added in new materials and throwing

--- Meaningful Lever/Assets/Scripts/ThrowItem.cs
+++ Meaningful Lever/Assets/Scripts/ThrowItem.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowItem : MonoBehaviour {
+public class ThrowItem : MonoBehaviour
+{
+    public GameObject throwItem;
+    public float force;
+	void Update ()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 dir = Camera.main.ScreenPointToRay(Input.mousePosition).direction.normalized;
+
+            GameObject go = Instantiate(throwItem, new Vector3(transform.position.x + dir.x, transform.position.y + dir.y, transform.position.z + dir.z), Quaternion.identity);
+            go.tag = "ThrownItem";
+            go.GetComponent<Rigidbody>().velocity = dir * force;
+            Debug.Log(new Vector3(transform.position.x - dir.normalized.x, transform.position.y + dir.normalized.y, transform.position.z - dir.normalized.z));
+        }
+
 	}
 }
